6d60b59561b2a802bcdf270af37614edfce06eee
urthbound <jeffowler@gmail.com>
2016-07-16 18:54:57 -0400

    swapoff and -s parted

--- prepscript.sh
+++ prepscript.sh
@@ -4,23 +4,24 @@
 
 wifi-menu
 
-umount /mnt/boot/efi
+umount /mnt/boot
 umount /mnt
+swapoff -a
 
 parted -s /dev/sda mklabel gpt
 
-parted /dev/sda mkpart msdos 2048s 411647s
-parted /dev/sda set 1 esp on
-parted /dev/sda set 1 boot on
+parted -s /dev/sda mkpart msdos 2048s 411647s
+parted -s /dev/sda set 1 esp on
+parted -s /dev/sda set 1 boot on
 mkfs.fat -F32 /dev/sda1 -F
 
-parted /dev/sda mkpart ext4 411648s 21383167s
+parted -s /dev/sda mkpart ext4 411648s 21383167s
 mkfs.ext4 /dev/sda2 -F
 
-parted /dev/sda mkpart ext4 21383168s 42354687s
+parted -s /dev/sda mkpart ext4 21383168s 42354687s
 mkfs.ext4 /dev/sda3 -F
 
-parted /dev/sda mkpart linux-swap 42354688s 46548991s
+parted -s /dev/sda mkpart linux-swap 42354688s 46548991s
 mkswap /dev/sda4
 swapon /dev/sda4
 
