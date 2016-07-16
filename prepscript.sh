b788556cff4c692ed70d6438160c04820ff774fa
urthbound <jeffowler@gmail.com>
2016-07-16 18:30:09 -0400

    swapon

--- prepscript.sh
+++ prepscript.sh
@@ -10,16 +10,19 @@ umount /mnt
 parted -s /dev/sda mklabel gpt
 
 parted /dev/sda mkpart msdos 2048s 411647s
-
 parted /dev/sda set 1 esp on
 parted /dev/sda set 1 boot on
+mkfs.fat -F32 /dev/sda1 -F
 
 parted /dev/sda mkpart ext4 411648s 21383167s
-parted /dev/sda mkpart ext4 21383168s 42354687s
-
-mkfs.fat -F32 /dev/sda1 -F
 mkfs.ext4 /dev/sda2 -F
+
+parted /dev/sda mkpart ext4 21383168s 42354687s
 mkfs.ext4 /dev/sda3 -F
+
+parted /dev/sda mkpart linux-swap 42354688s 46548991s
+mkswap /dev/sda4
+swapon /dev/sda4
 
 mount /dev/sda3 /mnt
 mkdir -p /mnt/boot
