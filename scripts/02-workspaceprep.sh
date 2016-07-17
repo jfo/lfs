56f597142c0fa5262b622e1849d64ddeba22aee5
Jeff Fowler <jeffowler@gmail.com>
2016-07-17 14:37:23 +0000

    fixin 02

--- scripts/02-workspaceprep.sh
+++ scripts/02-workspaceprep.sh
@@ -7,11 +7,12 @@ mount /dev/sda2 /mnt
 mkdir -v /mnt/lfs
 LFS=/mnt/lfs
 
-mkdir -v $LFS/src
-wget --input-file=../sources/wget-list --continue --directory-prefix=$LFS/src
+mkdir -v $LFS/sources
+chmod -v a+wt $LFS/sources
+wget --input-file=../sources/wget-list --continue --directory-prefix=$LFS/sources
 
-cp ../sources/checksums $LFS/src/
-pushd $LFS/src
+cp ../sources/checksums $LFS/sources/
+pushd $LFS/sources
 md5sum -c checksums
 popd
 
@@ -22,12 +23,16 @@ groupadd lfs
 useradd -s /bin/bash -g lfs -m -k /dev/null lfs
 passwd lfs
 chown -v lfs $LFS/tools
-chown -v lfs $LFS/src
+chown -v lfs $LFS/sources
 
 pushd ../bashrc
 bash rc-copy.sh
 popd
 
-su lfs -c "mkdir /home/lfs/lfs && git clone http://github.com/urthbound/lfs /home/lfs/lfs"
+su lfs -c "mkdir /home/lfs/lfs 						&&\
+	git clone https://github.com/urthbound/lfs /home/lfs/lfs 	&&\
+	git config --global user.name "Jeff Fowler"			&&\
+	git config --global user.email "jeffowler@gmail.com"
+
 rm -rf /root/lfs
 su - lfs
