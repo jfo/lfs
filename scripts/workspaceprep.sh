dd65c2d698d1f54aca4f9a2ce1c5f2a2a93e53c7
Jeff Fowler <jeffowler@gmail.com>
2016-07-17 03:39:11 +0000

    prepscript better

--- scripts/workspaceprep.sh
+++ scripts/workspaceprep.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
+git config --global user.name "Jeff Fowler"
+git config --global user.email "jeffowler@gmail.com"
+
+mount /dev/sda2 /mnt
+mkdir -v /mnt/lfs
+LFS=/mnt/lfs
+
 mkdir -v $LFS/src
 wget --input-file=../sources/wget-list --continue --directory-prefix=$LFS/src
-wget --input-file=../sources/md5sums --continue --directory-prefix=$LFS/src
-pushd $LFS/src
-md5sum -c md5sums
-popd
 
-mkdir -v $LFS/src/patches
-wget --input-file=../sources/patches --continue --directory-prefix=$LFS/src/patches
-wget --input-file=../sources/patchsums --continue --directory-prefix=$LFS/src/patches
-pushd $LFS/src/patches
-md5sum -c md5sums
+cp ../sources/checksums $LFS/src/
+pushd $LFS/src
+md5sum -c checksums
 popd
 
 mkdir -v $LFS/tools
@@ -21,5 +22,10 @@ groupadd lfs
 useradd -s /bin/bash -g lfs -m -k /dev/null lfs
 passwd lfs
 chown -v lfs $LFS/tools
-chown -v lfs $LFS/sources
+chown -v lfs $LFS/src
+
+pushd ../bashrc
+bash rc-copy.sh
+popd
+
 su - lfs
